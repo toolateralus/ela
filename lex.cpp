5eab53bf6c8d1ba1cef6bf27c20314736f989417
toolateralus <joshnjoshgaming@gmail.com>
2024-09-30 12:39:05 -0400

    added binary and hexadecimal literals to lexer.

--- lex.cpp
+++ lex.cpp
@@ -7,7 +7,7 @@ using std::string;
 using std::stringstream;
 
 void Lexer::get_token(State &state) {
-  
+
   size_t &pos = state.pos;
   const std::string &input = state.input;
   const size_t len = state.input_len;
@@ -52,7 +52,8 @@ void Lexer::get_token(State &state) {
         c = input[pos];
       }
       pos++;
-      state.lookahead_buffer.push_back(Token(location, token.str(), TType::String, TFamily::Literal));
+      state.lookahead_buffer.push_back(
+          Token(location, token.str(), TType::String, TFamily::Literal));
       return;
     }
 
@@ -64,10 +65,12 @@ void Lexer::get_token(State &state) {
       }
       string value = token.str();
       if (keywords.contains(value)) {
-        state.lookahead_buffer.push_back(Token(location, value, keywords.at(value), TFamily::Keyword));
+        state.lookahead_buffer.push_back(
+            Token(location, value, keywords.at(value), TFamily::Keyword));
         return;
       } else {
-        state.lookahead_buffer.push_back(Token(location, value, TType::Identifier, TFamily::Identifier));
+        state.lookahead_buffer.push_back(
+            Token(location, value, TType::Identifier, TFamily::Identifier));
         return;
       }
     } else if (std::ispunct(c)) {
@@ -82,9 +85,10 @@ void Lexer::get_token(State &state) {
           longest_match = current_match;
         } else {
           if (!longest_match.empty()) {
-            state.lookahead_buffer.push_back(Token(location, longest_match, operators.at(longest_match),
-                         TFamily::Operator));
-                         return;
+            state.lookahead_buffer.push_back(Token(location, longest_match,
+                                                   operators.at(longest_match),
+                                                   TFamily::Operator));
+            return;
           }
         }
         pos++;
@@ -92,9 +96,10 @@ void Lexer::get_token(State &state) {
       }
 
       if (!longest_match.empty()) {
-        state.lookahead_buffer.push_back(Token(location, longest_match, operators.at(longest_match),
-                     TFamily::Operator));
-                     return;
+        state.lookahead_buffer.push_back(Token(location, longest_match,
+                                               operators.at(longest_match),
+                                               TFamily::Operator));
+        return;
       } else {
         std::cout << "crisp: unable to lex operator: " << current_match
                   << std::endl;
@@ -102,16 +107,32 @@ void Lexer::get_token(State &state) {
       }
     } else if (std::isdigit(c)) {
       bool is_float = false;
+      bool is_hex = false;
+      bool is_bin = false;
       int startPos = pos;
-      while (pos < len && (std::isdigit(c) || c == '.')) {
+
+      if (c == '0' && (input[pos + 1] == 'x' || input[pos + 1] == 'X')) {
+        is_hex = true;
+        pos += 2; // Skip '0x'
+        c = input[pos];
+      } else if (c == '0' && (input[pos + 1] == 'b' || input[pos + 1] == 'B')) {
+        is_bin = true;
+        pos += 2; // Skip '0b'
+        c = input[pos];
+      }
+
+      while (pos < len &&
+             (std::isdigit(c) || c == '.' || (is_hex && std::isxdigit(c)) ||
+              (is_bin && (c == '0' || c == '1')))) {
         if (c == '.') {
           if (is_float) {
             auto str = token.str();
             str.pop_back();
             pos++;
             state.lookahead_buffer.emplace_back(location, "..", TType::Range,
-                                     TFamily::Operator);
-            state.lookahead_buffer.push_back(Token(location, str, TType::Integer, TFamily::Literal));
+                                                TFamily::Operator);
+            state.lookahead_buffer.push_back(
+                Token(location, str, TType::Integer, TFamily::Literal));
           }
           is_float = true;
         }
@@ -120,15 +141,20 @@ void Lexer::get_token(State &state) {
         c = input[pos];
       }
       auto value = token.str();
-
-      if (!is_float) {
-        state.lookahead_buffer.push_back(Token(location, value, TType::Integer, TFamily::Literal));
-        return;
+      
+      if (is_hex) {
+        state.lookahead_buffer.push_back(Token(location, "0x" + value, TType::Integer, TFamily::Literal));
+      } else if (is_bin) {
+        state.lookahead_buffer.push_back(
+            Token(location, "0b" + value, TType::Integer, TFamily::Literal));
+      } else if (!is_float) {
+        state.lookahead_buffer.push_back(
+            Token(location, value, TType::Integer, TFamily::Literal));
+      } else {
+        state.lookahead_buffer.push_back(
+            Token(location, value, TType::Float, TFamily::Literal));
       }
-      else {
-        state.lookahead_buffer.push_back(Token(location, value, TType::Float, TFamily::Literal));
-        return;
-      }
+      return;
     } else {
       std::cout << "crisp: unable to lex : " << c << std::endl;
       exit(1);
