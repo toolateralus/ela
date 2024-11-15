42ca01534a47d2cb862be89e2db25f7ccf6ee44d
toolateralus <109556963+toolateralus@users.noreply.github.com>
2024-11-14 19:14:55 -0500

    remove dumb json files i forgot to stop generating

--- src/main.cpp
+++ src/main.cpp
@@ -172,13 +172,13 @@ int CompileCommand::emit_code(ASTProgram *root, Context &context) {
 
     std::string extra_flags = compilation_flags;
 
-    if (has_flag("debug")) extra_flags += " -g ";
+    if (has_flag("debug")) extra_flags += " -g " ;// -ftime-trace for debugging C++ compilation times;
 
     static std::string ignored_warnings = "-w";
 
     std::string output_flag = (compilation_flags.find("-o") != std::string::npos) ? "" : "-o " + binary_path.string();
 
-    auto compilation_string = std::format("clang++ -std=c++23 -ftime-trace {} -L/usr/local/lib {} {} {}", ignored_warnings, output_path.string(), output_flag, extra_flags);
+    auto compilation_string = std::format("clang++ -std=c++23 {} -L/usr/local/lib {} {} {}", ignored_warnings, output_path.string(), output_flag, extra_flags);
 
     if (compile_command.has_flag("x"))
       printf("\e[1;36m%s\n\e[0m", compilation_string.c_str());
