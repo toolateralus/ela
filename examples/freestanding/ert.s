7a4e33620767ca7a4c9f093a07b2d4b72cef2302
toolateralus <109556963+toolateralus@users.noreply.github.com>
2024-12-27 12:11:08 -0500

    fix example

--- examples/freestanding/ert.s
+++ examples/freestanding/ert.s
@@ -1,6 +1,5 @@
 section .text
 
-
 global exit
 
 exit:
--- examples/freestanding/run.sh
+++ examples/freestanding/run.sh
@@ -1,4 +1,10 @@
-ela main.ela
+ela main.ela 
+
+if ! command -v nasm &> /dev/null
+then
+  echo -e "\e[31mWarning: nasm is not installed. Please install nasm to proceed, or rewrite the assembly for your target machine and use your own assembler\e[0m"
+  exit 1
+fi
 
 nasm -felf64 ert.s -o ert.o
 nasm -felf64 print.s -o print.o
