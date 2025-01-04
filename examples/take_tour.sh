f8cb9156c40f0ec89ee2a4b04b8b6950f8cea171
toolateralus <109556963+toolateralus@users.noreply.github.com>
2025-01-04 11:35:46 -0500

    Replace run.sh with ela run in example scripts and remove unused run.sh file

--- examples/take_tour.sh
+++ examples/take_tour.sh
@@ -1,11 +1,11 @@
 echo starting 'target practice'...
 cd target_practice > /dev/null
-./run.sh
+ela run
 
 echo done. starting 'sequencer'...
 cd ../sequencer > /dev/null
-./run.sh
+ela run
 
 echo done. starting 'dynamic library', a hot reload-able test using raylib.
 cd ../dynamic_library  > /dev/null
-./run.sh
+ela run
