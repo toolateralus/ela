323272eb1f051cd7900214260bd00ea02afbaafa
toolateralus <109556963+toolateralus@users.noreply.github.com>
2025-01-08 11:09:35 -0500

    Update .gitignore, refactor test paths, and add new tests for generics, aliases, and reflection

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,4 @@
-for test_file in ela_source/tests/*.ela; do
+for test_file in tests/*.ela; do
   echo -e "\e[1;33mRunning test: $test_file\e[0m"
   ela "$test_file" --test && ./"${test_file%.*}"
   if [ -f "${test_file%.ela}" ]; then
