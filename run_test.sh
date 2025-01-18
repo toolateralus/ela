1d6fe58c7e7abb8cb76ac305937b9248e3fd8a35
toolateralus <109556963+toolateralus@users.noreply.github.com>
2025-01-18 17:23:58 -0500

    made run_test fancier

--- run_test.sh
+++ run_test.sh
@@ -1,10 +1,23 @@
+#!/bin/bash
+
 trap "echo -e '\e[1;34m --testing cancelled-- \e[0m'; exit" SIGINT SIGTERM
+
+total_compile_time=0
+declare -a compile_times
+declare -a test_files
 
 if [ $# -gt 0 ]; then
   test_file="tests/$1.ela"
   if [ -f "$test_file" ]; then
     echo -e "\e[1;33mRunning test: $test_file\e[0m"
-    ela "$test_file" --test && ./"${test_file%.*}"
+    start_time=$(date +%s%N)
+    ela "$test_file" --test
+    end_time=$(date +%s%N)
+    compile_time=$(( (end_time - start_time) / 1000000 ))
+    compile_times+=($compile_time)
+    test_files+=("$test_file")
+    total_compile_time=$((total_compile_time + compile_time))
+    ./"${test_file%.*}"
     if [ -f "${test_file%.ela}" ]; then
       rm "${test_file%.ela}"
     fi
@@ -17,8 +30,23 @@ fi
 
 for test_file in tests/*.ela; do
   echo -e "\e[1;33mRunning test: $test_file\e[0m"
-  ela "$test_file" --test && ./"${test_file%.*}"
+  start_time=$(date +%s%N)
+  ela "$test_file" --test
+  end_time=$(date +%s%N)
+  compile_time=$(( (end_time - start_time) / 1000000 ))
+  compile_times+=($compile_time)
+  test_files+=("$test_file")
+  total_compile_time=$((total_compile_time + compile_time))
+  ./"${test_file%.*}"
   if [ -f "${test_file%.ela}" ]; then
     rm "${test_file%.ela}"
   fi
 done
+
+read -p "show compile times? [y/n] " show_compile_times
+if [[ "$show_compile_times" == "y" || "$show_compile_times" == "Y" ]]; then
+  for i in "${!compile_times[@]}"; do
+    echo -e "\e[1;36m\e[1m${test_files[$i]}\e[0m took \e[1;32m\e[1m${compile_times[$i]}ms\e[0m"
+  done
+  echo -e "\e[0mran \e[1;33m\e[1m${#compile_times[@]}\e[0m tests in \e[1;32m${total_compile_time}ms\e[0m"
+fi
