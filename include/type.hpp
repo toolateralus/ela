f211c4591aaa41b1f4f856acc9e6d89dbe0d8bed
cooper <cooper.pilot@hotmail.com>
2025-01-29 20:12:12 -0500

    fixed repros 74 and 75, better find tuple type

--- include/type.hpp
+++ include/type.hpp
@@ -243,7 +243,7 @@ int global_create_interface_type(const InternedString &name, Scope *scope,
 
 int global_create_tagged_union_type(const InternedString &name, Scope *scope, const std::vector<int> &generic_args);
 int global_create_enum_type(const InternedString &, Scope *, bool = false, size_t element_type = s32_type());
-int global_create_tuple_type(const std::vector<int> &types, const TypeExtensions &ext);
+int global_create_tuple_type(const std::vector<int> &types);
 ConversionRule type_conversion_rule(const Type *from, const Type *to, const SourceRange & = {});
 // char *
 int global_find_function_type_id(const FunctionTypeInfo &, const TypeExtensions &);
