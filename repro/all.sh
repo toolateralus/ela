93e8ea7e8d9a25e55a47de6f233391454ff13b0f
cooper <cooper.pilot@hotmail.com>
2025-02-09 14:20:48 -0500

    now moving fixed repros instead of deleting so we can verify they stay fixed

--- repro/all.sh
+++ repro/all.sh
@@ -1,30 +1,49 @@
 #!/bin/bash
 trap "echo -e '\e[1;34m --testing cancelled-- \e[0m'; exit" SIGINT SIGTERM
 
-passing=()
-failing=()
+fixed=()
+unfixed=()
 exec 3>&1 4>&2
 exec 1>/dev/null 2>/dev/null
 for file in *.ela; 
 do
 	base_name=$(basename "$file" .ela)
-	if ela "$file" && ./"$base_name"; then
+	if ela "$file"; then
+		if ./"$base_name"; then
+			fixed+=("$base_name")
+		fi
 		rm "$base_name"
-		passing+=("$base_name")
+	fi
+done
+for file in fixed/*.ela; 
+do
+	base_name=$(basename "$file" .ela)
+	if ! ela "$file" ; then
+		unfixed+=("$base_name")
 	else
-		failing+=("$base_name")
+		if ! ./"$base_name"; then
+			unfixed+=("$base_name")
+		fi
+		rm "$base_name"
 	fi
 done
 exec 1>&3 2>&4
 exec 3>&- 4>&-
-echo -e "\e[1;32mPassed: ${passing[@]}\e[0m"
-echo -e "\e[1;31mFailed: ${failing[@]}\e[0m"
-echo -e "\e[1;31m# of Failed: ${#failing[@]}\e[0m"
+echo -e "\e[0mFixed: \e[1;32m${fixed[@]}\e[0m"
+echo -e "\e[0mUnfixed: \e[1;31m${unfixed[@]}\e[0m"
 
-for test in "${passing[@]}"; do
-	read -p "Do you want to prune the passing test '$test'? [y/n] " choice
+for test in "${fixed[@]}"; do
+	read -p "Do you want to move the fixed test '$test'? [y/n] " choice
 	case "$choice" in 
-		y|Y ) rm "$test".ela; echo "Pruned $test";;
+		y|Y ) mv "$test".ela fixed/"$test".ela; echo "Moved $test";;
+		n|N ) echo "Kept $test";;
+		* ) echo "Invalid choice, kept $test";;
+	esac
+done
+for test in "${unfixed[@]}"; do
+	read -p "Do you want to move the unfixed test '$test'? [y/n] " choice
+	case "$choice" in 
+		y|Y ) mv fixed/"$test".ela "$test".ela; echo "Moved $test";;
 		n|N ) echo "Kept $test";;
 		* ) echo "Invalid choice, kept $test";;
 	esac
