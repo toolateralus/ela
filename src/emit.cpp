d3df7ad448c81accb5e19453c1292c94aee62c05
toolateralus <109556963+toolateralus@users.noreply.github.com>
2025-05-21 11:38:02 -0400

    refcator ./loc.py

--- src/emit.cpp
+++ src/emit.cpp
@@ -170,7 +170,7 @@ std::string Emitter::get_field_struct(const std::string &name, Type *type, Type 
       if (HAS_FLAG(flags, STRUCT_IS_FORWARD_DECLARED)) {
         ss << std::format(".size = sizeof({}), ", type_to_string(type));
       } else {
-        ss << ".size = 0, "; // non sized type
+        ss << ".size = 0, "; // ZST.
       }
     } else {
       ss << std::format(".size = sizeof({}), ", type_to_string(type));
