f4b4f4f9138aa2af0186b6146fd0a853c3c83bd7
toolateralus <109556963+toolateralus@users.noreply.github.com>
2025-09-30 16:59:14 -0400

    refactor: remove unused format constants and add compiler version identifier

--- include/strings.hpp
+++ include/strings.hpp
@@ -36,16 +36,8 @@ constexpr const char *DISCRIMINANT_KEY = "$discriminant";
 constexpr const char *OPTION_NONE_DISCRIMINANT_VALUE = "1";
 constexpr const char *OPTION_SOME_DISCRIMINANT_VALUE = "2";
 
-constexpr const char *THIR_RETURN_OVERRIDE_REGISTER_KEY_FORMAT = "$ror{}";
-constexpr const char *THIR_SWITCH_CACHED_EXPRESSION_KEY_FORMAT = "$switch_cached{}";
-
-constexpr const char *THIR_FOR_LOOP_ITER_OPTION_KEY_FORMAT = "$for_iter{}";
-constexpr const char *THIR_FOR_LOOP_ITER_CACHED_KEY_FORMAT = "$for_cached{}";
-
-constexpr const char *TYPE_INFO_IDENTIFIER_FORMAT = "$typeinfo{}";
-
-constexpr const char *THIR_PATTERN_MATCH_CACHED_KEY_FORMAT = "$patmatch{}";
-constexpr const char *THIR_DESTRUCTURE_CAHCED_VARIABLE_KEY_FORMAT = "$destructure{}";
+// if you publish a tag, this MUST match the tag identifier.
+constexpr const char *COMPILER_VERSION = "v0.0.0";
 
 // We should probably not rely on this since RangeBase is defined in the stdlib.
 constexpr const char *RANGE_TYPE_BEGIN_KEY = "begin";
